f1afd84fb4a0c42d32f398eedfd884f92c410184
benisamuel <benisamuel566@gmail.com>
2025-09-18 20:54:03 +0200

    added boarding data

---
+++ src/data/boarding/boarding.data.ts
@@ -0,0 +1,44 @@
+const boarding_data = [
+  {
+    id: 1,
+    type: "interactive",
+    question: `What languages do \n you want to use \n for Olilo?`,
+    options: ["United States", "French", "Kinyarwanda", "Spanish"],
+  },
+  {
+    id: 2,
+    type: "interactive",
+    question: "Why are you using \n Olilo?",
+    options: [
+      "To review books i read",
+      "Support my education",
+      "Vacation usage",
+      "Just for fun",
+      "Others reason",
+    ],
+  },
+  {
+    id: 3,
+    type: "interactive",
+    question: "This is what you can \n achieve with Olilo?",
+    options: [
+      "Record easily what you read from several books",
+      "Review easily what you read from several books",
+      "Cultivate reviewing habit",
+    ],
+  },
+  {
+    id: 4,
+    type: "interactive",
+    question: "What are your daily \n review time?",
+    options: [
+      "5 mins / day",
+      "10 mins / day",
+      "15 mins / day",
+      "30 mins / day",
+      "60 mins / day",
+    ],
+  },
+];
+
+export default boarding_data;

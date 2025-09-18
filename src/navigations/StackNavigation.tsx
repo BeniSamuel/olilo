567ca64c145fff00ca8961c2fe87a835fac8e3ac
benisamuel <benisamuel566@gmail.com>
2025-09-18 21:00:46 +0200

    added interactive boarding screens

--- src/navigations/StackNavigation.tsx
+++ src/navigations/StackNavigation.tsx
@@ -5,12 +5,14 @@ import Otp from "../screens/stacks/otp-screen/Otp";
 import Loading from "../screens/stacks/loading/Loading";
 import TabNavigation from "./TabNavigation";
 import Boarding from "../screens/stacks/boarding/Boarding";
+import InfoBoarding from "../screens/stacks/boarding/InfoBoarding";
 
 const Stack = createStackNavigator();
 
 const StackNavigation = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="InfoBoarding" component={InfoBoarding} />
       <Stack.Screen name="Boarding" component={Boarding} />
       <Stack.Screen name="Signin" component={Signin} />
       <Stack.Screen name="Signup" component={Signup} />

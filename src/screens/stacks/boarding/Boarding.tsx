50b56a450f88c2c848e03216a065f11c1cc94b68
benisamuel <benisamuel566@gmail.com>
2025-09-18 20:54:47 +0200

    added boarding stacks

--- src/screens/stacks/boarding/Boarding.tsx
+++ src/screens/stacks/boarding/Boarding.tsx
@@ -1,14 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Platform, StyleSheet, View, FlatList, Dimensions } from "react-native";
+import React, { useRef, useState } from "react";
+import TopBar from "../../../components/boardings/TopBar";
+import { bg_colors } from "../../../theme/color.theme";
+import dimensions from "../../../theme/dimension.theme";
+import boarding_data from "../../../data/boarding/boarding.data";
+import InteractiveBoarding from "../../../components/boardings/InteractiveBoarding";
+
+const { width, height } = Dimensions.get("window");
 
 const Boarding = () => {
+  const flatListRef = useRef<FlatList>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    if (currentIndex < boarding_data.length - 1) {
+      flatListRef.current?.scrollToIndex({ index: currentIndex + 1 });
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const renderItem = ({ item }) => {
+    return <InteractiveBoarding {...item} style={styles.page} />;
+  };
+
   return (
-    <View>
-      <Text>Welcome</Text>
+    <View style={styles.container}>
+      <FlatList
+        ref={flatListRef}
+        data={boarding_data}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={renderItem}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onMomentumScrollEnd={(ev) => {
+          const index = Math.round(ev.nativeEvent.contentOffset.x / width);
+          setCurrentIndex(index);
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Boarding
+export default Boarding;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: bg_colors.main_bg,
+    paddingTop: Platform.OS === "android" ? dimensions.height * 0.035 : 0,
+  },
+  page: {
+    width,
+    height,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ src/screens/stacks/boarding/InfoBoarding.tsx
@@ -0,0 +1,108 @@
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import dimensions from "../../../theme/dimension.theme";
+import { bg_colors, text } from "../../../theme/color.theme";
+import { useNavigation } from "@react-navigation/native";
+
+const InfoBoarding = () => {
+    const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <View style={styles.greeting_container}>
+        <Text style={styles.greeting_text}>Hi there , I'm Oli!</Text>
+      </View>
+      <View style={styles.description_container}>
+        <Image source={require("../../../../assets/boarding/oli_icon.png")} />
+        <View style={styles.text_container}>
+          <Text style={styles.title}>Olilo</Text>
+          <Text style={styles.description_text}>
+            Keep your book insights alive {"\n"}
+            review them anytime, anywhere with {"\n"}
+            Olilo's simple and engaging features. Enjoy
+          </Text>
+        </View>
+      </View>
+      <View style={styles.button_container}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Boarding")}>
+          <Text style={styles.button_text}>GET STARTED</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default InfoBoarding;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Platform.OS === "android" ? dimensions.height * 0.15 : null,
+    backgroundColor: bg_colors.main_bg,
+    flex: 1,
+    paddingHorizontal: dimensions.width * 0.045,
+    paddingBottom: 12,
+    display: "flex",
+    gap: dimensions.height * 0.1,
+  },
+  greeting_container: {
+    backgroundColor: "white",
+    borderColor: bg_colors.button_bg_inactive,
+    borderWidth: 2,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 15,
+    borderRadius: 12,
+  },
+  greeting_text: {
+    color: text.color,
+    fontFamily: "montserrat-semibold",
+  },
+  title: {
+    color: bg_colors.button_bg_active,
+    fontFamily: "montserrat-bold",
+    fontSize: 20,
+  },
+  description_text: {
+    color: text.color,
+    fontFamily: "montserrat-medium",
+    lineHeight: 25,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: bg_colors.button_bg_active,
+    paddingHorizontal: 12,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 14,
+    borderRadius: 25,
+  },
+  button_text: {
+    color: text.button_text,
+    fontFamily: "montserrat-semibold",
+  },
+  description_container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: 12,
+  },
+  text_container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: 12,
+  },
+  button_container: {
+    paddingVertical: 12,
+  }
+});

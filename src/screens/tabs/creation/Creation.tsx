a08c318bfe7ef6f5d1f5470627d8513131116946
benisamuel <benisamuel566@gmail.com>
2025-09-18 20:58:20 +0200

    added creation screen tab

---
+++ src/screens/tabs/creation/Creation.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Creation = () => {
+  return (
+    <View>
+      <Text>Creation</Text>
+    </View>
+  )
+}
+
+export default Creation
+
+const styles = StyleSheet.create({})

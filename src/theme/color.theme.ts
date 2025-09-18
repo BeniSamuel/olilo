62045d762fcab698fee5999d2e8ee9e826a1080a
benisamuel <benisamuel566@gmail.com>
2025-09-18 20:59:45 +0200

    added dimension and color theme

---
+++ src/theme/color.theme.ts
@@ -0,0 +1,11 @@
+export const bg_colors = {
+  main_bg: "#FFF9F9",
+  button_bg_active: "#F88C2D",
+  button_bg_inactive: "rgba(121, 117, 113, 0.09)",
+};
+
+export const text = {
+  color: "#797571",
+  headers: "#000000",
+  button_text: "#ffffff",
+};
---
+++ src/theme/dimension.theme.ts
@@ -0,0 +1,8 @@
+import { Dimensions } from "react-native";
+
+const dimensions = {
+  height: Dimensions.get("window").height,
+  width: Dimensions.get("window").width,
+};
+
+export default dimensions;

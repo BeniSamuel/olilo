556b3ca2842752a3dcd0de49a11ce5261bfa5778
benisamuel <benisamuel566@gmail.com>
2025-09-19 15:14:00 +0200

    Finished interactive boardings

--- src/screens/stacks/boarding/Boarding.tsx
+++ src/screens/stacks/boarding/Boarding.tsx
@@ -1,26 +1,40 @@
 import { Platform, StyleSheet, View, FlatList, Dimensions } from "react-native";
-import React, { useRef, useState } from "react";
-import TopBar from "../../../components/boardings/TopBar";
+import React, { useRef } from "react";
 import { bg_colors } from "../../../theme/color.theme";
 import dimensions from "../../../theme/dimension.theme";
 import boarding_data from "../../../data/boarding/boarding.data";
 import InteractiveBoarding from "../../../components/boardings/InteractiveBoarding";
+import { useBoardingStore } from "../../../store/boarding_store/boarding.store";
 
 const { width, height } = Dimensions.get("window");
 
 const Boarding = () => {
   const flatListRef = useRef<FlatList>(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  let { section, setSection } = useBoardingStore();
 
   const handleNext = () => {
-    if (currentIndex < boarding_data.length - 1) {
-      flatListRef.current?.scrollToIndex({ index: currentIndex + 1 });
-      setCurrentIndex(currentIndex + 1);
+    if (section < boarding_data.length - 1) {
+      flatListRef.current?.scrollToIndex({ index: section + 1, animated: true });
+      setSection(section + 1);
     }
   };
 
+  const handleBack = () => {
+  if (section > 0) {
+    flatListRef.current?.scrollToIndex({ index: section - 1, animated: true });
+    setSection(section - 1);
+  }
+};
+
   const renderItem = ({ item }) => {
-    return <InteractiveBoarding {...item} style={styles.page} />;
+    return (
+      <InteractiveBoarding
+        {...item}
+        handleNext={handleNext}
+        handleBack={handleBack}
+        style={styles.page}
+      />
+    );
   };
 
   return (
@@ -35,7 +49,7 @@ const Boarding = () => {
         showsHorizontalScrollIndicator={false}
         onMomentumScrollEnd={(ev) => {
           const index = Math.round(ev.nativeEvent.contentOffset.x / width);
-          setCurrentIndex(index);
+          setSection(index);
         }}
       />
     </View>
@@ -48,7 +62,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: bg_colors.main_bg,
-    paddingTop: Platform.OS === "android" ? dimensions.height * 0.035 : 0,
+    paddingVertical: dimensions.height * 0.035,
   },
   page: {
     width,

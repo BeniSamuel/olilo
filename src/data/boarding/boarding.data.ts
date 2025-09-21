784e4e6ceb5f660ee975f1d46ffaa7e44a30acb4
benisamuel <benisamuel566@gmail.com>
2025-09-21 21:39:30 +0200

    added icon support in data

--- src/data/boarding/boarding.data.ts
+++ src/data/boarding/boarding.data.ts
@@ -3,18 +3,50 @@ const boarding_data = [
     id: 1,
     type: "interactive",
     question: `What languages do \n you want to use \n for Olilo?`,
-    options: ["United States", "French", "Kinyarwanda", "Spanish"],
+    options: [
+      {
+        country: "United State",
+        icon: require("../../../assets/boarding/american_flag.png"),
+      },
+      {
+        country: "French",
+        icon: require("../../../assets/boarding/france_flag.png"),
+      },
+      {
+        country: "Kinyarwanda",
+        icon: require("../../../assets/boarding/rwanda_flag.png"),
+      },
+      {
+        country: "Spanish",
+        icon: require("../../../assets/boarding/spanish_flag.png"),
+      },
+    ],
   },
   {
     id: 2,
     type: "interactive",
     question: "Why are you using \n Olilo?",
     options: [
-      "To review books i read",
-      "Support my education",
-      "Vacation usage",
-      "Just for fun",
-      "Others reason",
+      {
+        country: "To review books i read",
+        icon: require("../../../assets/boarding/book_icon.png"),
+      },
+      {
+        country: "Support my education",
+        icon: require("../../../assets/boarding/graduation_icon.png"),
+      },
+      {
+        country: "Vacation usage",
+        icon: require("../../../assets/boarding/plane_icon.png"),
+      },
+      {
+        country: "Just for fun",
+        icon: require("../../../assets/boarding/just_fun.png"),
+      },
+      {
+        country: "Others reason",
+        icon: require("../../../assets/boarding/reason.png"),
+      },
     ],
   },
   {
@@ -22,9 +54,21 @@ const boarding_data = [
     type: "interactive",
     question: "This is what you can \n achieve with Olilo?",
     options: [
-      "Record easily what you read from several books",
-      "Review easily what you read from several books",
-      "Cultivate reviewing habit",
+      {
+        icon: require("../../../assets/boarding/record.png"),
+        topic: "Record easily what you\nread from several books",
+        subject: "Write key points from the books\nand your own insights",
+      },
+      {
+        icon: require("../../../assets/boarding/review.png"),
+        topic: "Review easily what you\nread from several books",
+        subject: "Review the key points your\nwrote from several books",
+      },
+      {
+        icon: require("../../../assets/boarding/reminder.png"),
+        topic: "Cultivate reviewing habit",
+        subject: "Smart reminders and fun challenges.",
+      },
     ],
   },
   {
@@ -32,11 +76,11 @@ const boarding_data = [
     type: "interactive",
     question: "What are your daily \n review time?",
     options: [
-      "5 mins / day",
-      "10 mins / day",
-      "15 mins / day",
-      "30 mins / day",
-      "60 mins / day",
+      { time: "5 mins / day", status: "Relax" },
+      { time: "10 mins / day", status: "Normal" },
+      { time: "15 mins / day", status: "Serious" },
+      { time: "30 mins / day", status: "Great" },
+      { time: "60 mins / day", status: "Wow" },
     ],
   },
 ];

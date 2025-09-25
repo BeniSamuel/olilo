f7199c2cc041835ea29cf9d878a361ea0c26a41e
benisamuel <benisamuel566@gmail.com>
2025-09-25 12:10:54 +0200

    create success screen status

--- src/screens/stacks/status/Status.tsx
+++ src/screens/stacks/status/Status.tsx
@@ -1,14 +1,116 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import dimensions from "../../../theme/dimension.theme";
+import { bg_colors, text } from "../../../theme/color.theme";
+import { useNavigation } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
 
 const Status = () => {
+    const navigation = useNavigation();
   return (
-    <View>
-      <Text>Status</Text>
+    <View style={styles.container}>
+      <StatusBar style="auto"/>
+      <View style={styles.greeting_container}>
+        <Text style={styles.greeting_text}>Awesome!</Text>
+      </View>
+      <View style={styles.description_container}>
+        <Image source={require("../../../../assets/status/oli_icon_happy.png")} resizeMode="contain"/>
+        <View style={styles.text_container}>
+          <Text style={styles.description_text}>
+            Create a profile now so you can save {"\n"}
+            progress and connect with friends {"\n"}
+            Or you can skip it.
+          </Text>
+        </View>
+      </View>
+      <View style={styles.button_container}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Signup")}>
+          <Text style={styles.button_text}>CREATE PROFILE</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, { backgroundColor: bg_colors.button_bg_inactive}]} onPress={() => navigation.navigate("Signin")}>
+          <Text style={[styles.button_text, { color: bg_colors.button_bg_active}]}>SKIP</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Status
+export default Status;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: dimensions.height * 0.15,
+    backgroundColor: bg_colors.main_bg,
+    flex: 1,
+    paddingHorizontal: dimensions.width * 0.045,
+    paddingBottom: 12,
+    display: "flex",
+    flexDirection: "column",
+    gap: dimensions.height * 0.01,
+    justifyContent: "space-between"
+  },
+  greeting_container: {
+    backgroundColor: "white",
+    borderColor: bg_colors.button_bg_inactive,
+    borderWidth: 2,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 15,
+    borderRadius: 12,
+  },
+  greeting_text: {
+    color: text.color,
+    fontFamily: "montserrat-semibold",
+  },
+  title: {
+    color: bg_colors.button_bg_active,
+    fontFamily: "montserrat-bold",
+    fontSize: 20,
+  },
+  description_text: {
+    color: text.color,
+    fontFamily: "montserrat-medium",
+    lineHeight: 25,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: bg_colors.button_bg_active,
+    paddingHorizontal: 12,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 14,
+    borderRadius: 25,
+  },
+  button_text: {
+    color: text.button_text,
+    fontFamily: "montserrat-semibold",
+  },
+  description_container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: 12,
+  },
+  text_container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: 12,
+  },
+  button_container: {
+    paddingVertical: 12,
+    display: "flex",
+    flexDirection: "column",
+    gap: 12
+  }
+});

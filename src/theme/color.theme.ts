7250d28c8c6a719bdb455fb14de1c6a0dba04dd9
benisamuel <benisamuel566@gmail.com>
2025-09-25 12:07:54 +0200

    added used gray_color

--- src/theme/color.theme.ts
+++ src/theme/color.theme.ts
@@ -2,6 +2,7 @@ export const bg_colors = {
   main_bg: "#FFF9F9",
   button_bg_active: "#F88C2D",
   button_bg_inactive: "rgba(121, 117, 113, 0.09)",
+  gray_color: "#D9D9D9"
 };
 
 export const text = {

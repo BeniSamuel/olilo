94e03cf8ec6f9f65f5f191aa2898e444c9ce8b8b
benisamuel <benisamuel566@gmail.com>
2025-09-30 15:26:13 +0200

    created booking data

---
+++ src/data/books/book.data.ts
@@ -0,0 +1,22 @@
+const books = [
+  {
+    id: 1,
+    name: "Atomic Habits",
+    status_description: "3 Notes Reviewed",
+    status: "done",
+  },
+  {
+    id: 2,
+    name: "Deep Works",
+    status_description: "2 Pending",
+    status: "doing",
+  },
+  {
+    id: 3,
+    name: "Good Money Habits",
+    status_description: "Not Yet",
+    status: "not_done",
+  },
+];
+
+export default books;

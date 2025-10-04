65606ea4e386e937b40c4367c551d08f43b030d4
benisamuel <benisamuel566@gmail.com>
2025-10-04 17:47:24 +0200

    added badges used data

---
+++ src/data/badge/badge.data.ts
@@ -0,0 +1,14 @@
+const badges = [
+  {
+    id: 1,
+    title: "Quiz King",
+    date: new Date(),
+  },
+  {
+    id: 2,
+    title: "Mad Man",
+    date: new Date(),
+  },
+];
+
+export default badges;

f1f5e281b75e59c09ffa2f4d09b7ab225aa1e981
benisamuel <benisamuel566@gmail.com>
2025-10-04 17:48:33 +0200

    added other tab top bar

--- src/layout/TabLayout.tsx
+++ src/layout/TabLayout.tsx
@@ -2,15 +2,18 @@ import { StyleSheet, Text, View, ScrollView } from "react-native";
 import React, { ReactNode } from "react";
 import TopBar from "../components/home/TopBar";
 import { bg_colors } from "../theme/color.theme";
+import BottomBarTop from "../components/common/BottomBarTop";
 
 type TabLayoutProp = {
   children: ReactNode;
+  tab_name?: string;
 };
 
-const TabLayout: React.FC<TabLayoutProp> = ({ children }) => {
+const TabLayout: React.FC<TabLayoutProp> = ({ children, tab_name }) => {
   return (
     <View style={styles.container}>
-      <TopBar />
+      {tab_name !== "home" && <BottomBarTop bar_name={tab_name} />}
+      {tab_name === "home" && <TopBar />}
       <ScrollView showsVerticalScrollIndicator={false}>{children}</ScrollView>
     </View>
   );
@@ -19,8 +22,8 @@ const TabLayout: React.FC<TabLayoutProp> = ({ children }) => {
 export default TabLayout;
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: bg_colors.main_bg,
-        flex: 1
-    }
+  container: {
+    backgroundColor: bg_colors.main_bg,
+    flex: 1,
+  },
 });

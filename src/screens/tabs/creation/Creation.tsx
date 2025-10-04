29eeb03727b8e65a566ead4255c4a737b5d1724b
benisamuel <benisamuel566@gmail.com>
2025-10-04 17:51:35 +0200

    created creation screen tab first version

--- src/screens/tabs/creation/Creation.tsx
+++ src/screens/tabs/creation/Creation.tsx
@@ -1,11 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import TabLayout from '../../../layout/TabLayout'
+import Option from '../../../components/creation/Option'
+import useOptionStore from '../../../store/creation_store/option.store'
+import Review from '../../../components/creation/Review'
+import ChallengeCreation from '../../../components/creation/ChallengeCreation'
 
 const Creation = () => {
+  const { focused } = useOptionStore()
   return (
-    <View>
-      <Text>Creation</Text>
-    </View>
+    <TabLayout tab_name='Creation'>
+      <Option />
+      <View>
+        { focused === "review" ? <Review /> : <ChallengeCreation />}
+      </View>
+    </TabLayout>
   )
 }
 

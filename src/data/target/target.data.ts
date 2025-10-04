0c9cc9591a8b92d00c8e5c0e48f756c33a442571
benisamuel <benisamuel566@gmail.com>
2025-10-04 17:47:43 +0200

    added target used data

---
+++ src/data/target/target.data.ts
@@ -0,0 +1,28 @@
+const targets = [
+  {
+    id: 1,
+    mission: "Get 25 Diamonds",
+    progress: 1,
+    period: 7,
+  },
+  {
+    id: 2,
+    mission: "Get 40 XP",
+    progress: 4,
+    period: 7,
+  },
+  {
+    id: 3,
+    mission: "Get 25 Diamonds",
+    progress: 1,
+    period: 4,
+  },
+  {
+    id: 4,
+    mission: "Make a challenge",
+    progress: 1,
+    period: 2,
+  },
+];
+
+export default targets;

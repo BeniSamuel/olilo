00d830181227b308f79601e92fe576f195e6493b
benisamuel <benisamuel566@gmail.com>
2025-10-07 21:26:23 +0200

    working on static component

--- src/screens/tabs/account/Account.tsx
+++ src/screens/tabs/account/Account.tsx
@@ -1,8 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import TabLayout from '../../../layout/TabLayout'
 import UserProfile from '../../../components/account/UserProfile'
 import FollowingStats from '../../../components/account/FollowingStats'
+import { bg_colors, text } from '../../../theme/color.theme'
+import dimensions from '../../../theme/dimension.theme'
+import YourStatistics from '../../../components/account/YourStatistics'
+import YourAchievements from '../../../components/account/YourAchievements'
 
 const Account = () => {
   return (
@@ -11,10 +15,40 @@ const Account = () => {
         <UserProfile />
         <FollowingStats />
       </View>
+
+      <View style={styles.button_container}>
+        <TouchableOpacity style={styles.edit_button}>
+          <Text style={styles.button_text}>Edit Profile</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <YourStatistics />
+        <YourAchievements />
+      </View>
+
     </TabLayout>
   )
 }
 
 export default Account
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  button_container: {
+    paddingHorizontal: dimensions.width * 0.035,
+    paddingVertical: dimensions.height * 0.025
+  },
+  edit_button: {
+    backgroundColor: bg_colors.button_bg_active,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: dimensions.height * 0.015,
+    borderRadius: 20
+  },
+  button_text: {
+    color: text.button_text,
+    fontFamily: "montserrat-medium"
+  }
+})

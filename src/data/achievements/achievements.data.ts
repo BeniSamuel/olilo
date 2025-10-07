b005d0482d9bb9cdabd52a981c365668275b6869
benisamuel <benisamuel566@gmail.com>
2025-10-07 21:20:59 +0200

    added achievments data

---
+++ src/data/achievements/achievements.data.ts
@@ -0,0 +1,40 @@
+const achievements = [
+  {
+    id: 1,
+    count: 5,
+    category: "Streaks",
+    icon: require("../../../assets/account/streaks.png"),
+  },
+  {
+    id: 2,
+    count: 5,
+    category: "Streaks",
+    icon: require("../../../assets/account/streaks.png"),
+  },
+  {
+    id: 3,
+    count: 12,
+    category: "Diamonds",
+    icon: require("../../../assets/account/diamonds.png"),
+  },
+  {
+    id: 4,
+    count: 5,
+    category: "Streaks",
+    icon: require("../../../assets/account/streaks.png"),
+  },
+  {
+    id: 5,
+    count: 5,
+    category: "Streaks",
+    icon: require("../../../assets/account/streaks.png"),
+  },
+  {
+    id: 6,
+    count: 5,
+    category: "Streaks",
+    icon: require("../../../assets/account/streaks.png"),
+  },
+];
+
+export default achievements;

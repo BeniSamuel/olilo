f37e57718b3eaeb6e8fa0ccc48abde8eff2c346c
benisamuel <benisamuel566@gmail.com>
2025-10-08 21:43:19 +0200

    added statistics data

---
+++ src/data/statistics/statistics.data.ts
@@ -0,0 +1,16 @@
+const statistics = [
+  {
+    id: 1,
+    icon: require("../../../assets/account/great_king.png"),
+    title: "Great King",
+    description: "Get 5000 XP in this month\nto get achievements.",
+  },
+  {
+    id: 2,
+    icon: require("../../../assets/account/brain.png"),
+    title: "Einstein's Brain",
+    description: "Complete difficult\nchallenges 20 XP to earn this",
+  },
+];
+
+export default statistics;

082b62e08983bc72aa627b14cb95f446df6f2703
benisamuel <benisamuel566@gmail.com>
2025-10-09 16:21:18 +0200

    working on signup stack screen

--- src/screens/stacks/signup/Signup.tsx
+++ src/screens/stacks/signup/Signup.tsx
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, ListRenderItem, StyleSheet, Text, View } from 'react-native'
+import React, { useRef } from 'react'
+import TopBar from '../../../components/signup/TopBar'
+import { bg_colors } from '../../../theme/color.theme'
+import signup_data from '../../../data/signup/signup.data'
+import Form from '../../../components/signup/Form'
+
+interface itemType {
+  id: number;
+  question: string;
+}
 
 const Signup = () => {
+  const flatListRef = useRef<FlatList>(null);
+
+  const renderItem: ListRenderItem<itemType> = ({item}) => {
+    return (
+      <Form question={item.question} />
+    )
+  }
+
   return (
-    <View>
-      <Text>Signup</Text>
+    <View style={styles.container}>
+      <TopBar />
+      <View>
+        <FlatList 
+          ref={flatListRef}
+          data={signup_data}
+          keyExtractor={(_: {id: number, question: string},index) => index.toString()}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          renderItem={renderItem}
+        />
+      </View>
     </View>
   )
 }
 
 export default Signup
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: bg_colors.main_bg
+  }
+})

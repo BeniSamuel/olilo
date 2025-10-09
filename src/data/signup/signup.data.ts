18c5910e878381748006515e2a59991ce4f1742b
benisamuel <benisamuel566@gmail.com>
2025-10-09 16:19:17 +0200

    created signup data

---
+++ src/data/signup/signup.data.ts
@@ -0,0 +1,20 @@
+const signup_data = [
+  {
+    id: 1,
+    question: "What is your name?",
+  },
+  {
+    id: 2,
+    question: "How old are you?",
+  },
+  {
+    id: 3,
+    question: "What is your email\nAddress?",
+  },
+  {
+    id: 4,
+    question: "Create a password",
+  },
+];
+
+export default signup_data;

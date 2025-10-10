1148e8b2c9e96df6652b7a253b46a09a86631e7f
benisamuel <benisamuel566@gmail.com>
2025-10-10 21:02:36 +0200

    added review data

---
+++ src/data/review/review.data.ts
@@ -0,0 +1,67 @@
+const reviews = [
+  {
+    id: 1,
+    bookTitle: "Atomic Habits",
+    keyPoints: [
+      {
+        id: 1,
+        chapter: 1,
+        content: "I will never do things which I didn't plan"
+      },
+      {
+        id: 2,
+        chapter: 1,
+        content: "I will never do things which I didn't plan"
+      },
+      {
+        id: 3,
+        chapter: 1,
+        content: "I will never do things which I didn't plan"
+      },
+    ],
+  },
+  {
+    id: 2,
+    bookTitle: "Atomic Habits",
+    keyPoints: [
+      {
+        id: 1,
+        chapter: 1,
+        content: "I will never do things which I didn't plan"
+      },
+      {
+        id: 2,
+        chapter: 1,
+        content: "I will never do things which I didn't plan"
+      },
+      {
+        id: 3,
+        chapter: 1,
+        content: "I will never do things which I didn't plan"
+      },
+    ],
+  },
+  {
+    id: 3,
+    bookTitle: "Atomic Habits",
+    keyPoints: [
+      {
+        id: 1,
+        chapter: 1,
+        content: "I will never do things which I didn't plan"
+      },
+      {
+        id: 2,
+        chapter: 1,
+        content: "I will never do things which I didn't plan"
+      },
+      {
+        id: 3,
+        chapter: 1,
+        content: "I will never do things which I didn't plan"
+      },
+    ],
+  },
+];
+
+export default reviews;

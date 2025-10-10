ca9b8eb87faef38a4d2a653f5bfdf0854d1db709
benisamuel <benisamuel566@gmail.com>
2025-10-10 21:04:28 +0200

    added a review screen

--- src/screens/stacks/review/Review.tsx
+++ src/screens/stacks/review/Review.tsx
@@ -1,14 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { bg_colors } from '../../../theme/color.theme'
+import { StatusBar } from 'expo-status-bar'
+import dimensions from '../../../theme/dimension.theme'
+import TopReview from '../../../components/review/TopReview'
 
 const Review = () => {
   return (
-    <View>
-      <Text>Review</Text>
+    <View style={styles.container}>
+      <StatusBar style='dark'/>
+      <TopReview />
     </View>
   )
 }
 
 export default Review
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: bg_colors.main_bg,
+    paddingVertical: dimensions.height * 0.035,
+    paddingHorizontal: dimensions.width * 0.035
+  }
+})

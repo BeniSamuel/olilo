267c898943b3ca377b518ad4e5769276e85bfad1
benisamuel <benisamuel566@gmail.com>
2025-10-10 21:05:04 +0200

    added back and next functions

--- src/screens/stacks/signup/Signup.tsx
+++ src/screens/stacks/signup/Signup.tsx
@@ -4,6 +4,8 @@ import TopBar from '../../../components/signup/TopBar'
 import { bg_colors } from '../../../theme/color.theme'
 import signup_data from '../../../data/signup/signup.data'
 import Form from '../../../components/signup/Form'
+import useSignupStoreType from '../../../store/signup_store/signup.store'
+import { useNavigation } from '@react-navigation/native'
 
 interface itemType {
   id: number;
@@ -12,17 +14,38 @@ interface itemType {
 
 const Signup = () => {
   const flatListRef = useRef<FlatList>(null);
+  const { section, setSection, logout } = useSignupStoreType();
+  const navigation = useNavigation();
+
+  const handleNext = () => {
+    if (section < signup_data.length - 1 && section !== 4) {
+      flatListRef.current?.scrollToIndex({ index: section + 1, animated: true })
+      setSection(section + 1)
+    } else {
+      navigation.navigate("AfterSignup")
+      logout();
+    }
+  }
+
+  const handleBack = () => {
+    if (section > 0) {
+      flatListRef.current?.scrollToIndex({ index: section - 1, animated: true });
+      setSection(section -1)
+    } else {
+      navigation.goBack()
+    }
+  }
 
   const renderItem: ListRenderItem<itemType> = ({item}) => {
     return (
-      <Form question={item.question} />
+      <Form question={item.question} handleNext={handleNext}/>
     )
   }
 
   return (
     <View style={styles.container}>
-      <TopBar />
-      <View>
+      <TopBar handleBack={handleBack}/>
+      <View style={styles.content_container}>
         <FlatList 
           ref={flatListRef}
           data={signup_data}
@@ -43,5 +66,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: bg_colors.main_bg
+  },
+  content_container: {
+    flex: 1
   }
 })

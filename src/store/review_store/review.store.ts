19c850363a0e97f2dbe3f4c1c02ad3a72d8b5207
benisamuel <benisamuel566@gmail.com>
2025-10-11 19:23:00 +0200

    added logout for removing review section state

--- src/store/review_store/review.store.ts
+++ src/store/review_store/review.store.ts
@@ -1,2 +1,15 @@
+import { create } from "zustand";
+
 type ReviewStoreType = {
-}
+  section: number;
+  setSection: (section: ReviewStoreType["section"]) => void;
+  logout: () => void;
+};
+
+const useReviewStore = create<ReviewStoreType>((set) => ({
+  section: 0,
+  setSection: (section) => set({ section }),
+  logout: () => ({ section: 0 }),
+}));
+
+export default useReviewStore;

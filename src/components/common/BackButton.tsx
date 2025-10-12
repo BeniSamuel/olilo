64fec16b15c2e2ca7227f02c5814ae6ee016b668
benisamuel <benisamuel566@gmail.com>
2025-10-12 22:05:30 +0200

    added back button component

---
+++ src/components/common/BackButton.tsx
@@ -0,0 +1,25 @@
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import React from "react";
+import dimensions from "../../theme/dimension.theme";
+
+type BackButtonProp = {
+  handleBack: () => void;
+}
+
+const BackButton: React.FC<BackButtonProp> = ({ handleBack }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handleBack}>
+        <Image source={require("../../../assets/common/back-icon.png")} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BackButton;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: dimensions.height * 0.035,
+  },
+});

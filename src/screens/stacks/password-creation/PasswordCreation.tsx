b612ef247ae1acf3a07b7e94f6a8a50ff3c171da
benisamuel <benisamuel566@gmail.com>
2025-10-12 22:08:07 +0200

    added password creation stack screen

---
+++ src/screens/stacks/password-creation/PasswordCreation.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PasswordCreation = () => {
+  return (
+    <View>
+      <Text>PasswordCreation</Text>
+    </View>
+  )
+}
+
+export default PasswordCreation
+
+const styles = StyleSheet.create({})

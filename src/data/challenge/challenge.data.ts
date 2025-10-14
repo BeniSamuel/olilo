0c56ffe785e3fd2ad37e3d5ba4047dbdc6bd2eae
benisamuel <benisamuel566@gmail.com>
2025-10-14 21:53:45 +0200

    added challenges data to be used

---
+++ src/data/challenge/challenge.data.ts
@@ -0,0 +1,69 @@
+const challenges = [
+  {
+    id: 1,
+    title: "Quiz King",
+    questions: [
+      {
+        id: 1,
+        problem: "I will do things which i didn't plan",
+        category: "Complete the following question",
+        options: [
+          {
+            id: 1,
+            answer: "target",
+            result: "incorrect",
+          },
+          {
+            id: 2,
+            answer: "never",
+            result: "correct",
+          },
+          {
+            id: 3,
+            answer: "again",
+            result: "incorrect",
+          },
+          {
+            id: 4,
+            answer: "simple",
+            result: "incorrect",
+          },
+          {
+            id: 5,
+            answer: "ever",
+            result: "incorrect",
+          },
+          {
+            id: 6,
+            answer: "something",
+            result: "incorrect",
+          },
+        ],
+      },
+      {
+        id: 2,
+        problem: "I will never do things which i didn't plan",
+        category: "What does this sentence mean?",
+        options: [
+          {
+            id: 1,
+            answer: "I will enjoy doing everything that i want\nanytime without pressure",
+            result: "incorrect",
+          },
+          {
+            id: 2,
+            answer: "I will do things which I planned to ensure\nI use my time well without misusing it",
+            result: "correct",
+          },
+          {
+            id: 3,
+            answer: "I will never mind anything that i am doing",
+            result: "incorrect",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default challenges;

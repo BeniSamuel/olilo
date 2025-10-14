89536bba71952ed44d3de137788aac482145e319
benisamuel <benisamuel566@gmail.com>
2025-10-14 21:58:21 +0200

    added challenge store

--- src/store/challenge_store/challenge.store.ts
+++ src/store/challenge_store/challenge.store.ts
@@ -2,11 +2,15 @@ import { create } from "zustand";
 
 interface ChallengeStoreType {
   focused: string;
+  section: number;
+  setSection: (section: ChallengeStoreType["section"]) => void;
   setFocused: (focused: ChallengeStoreType["focused"]) => void;
 }
 
 const useChallengeStore = create<ChallengeStoreType>((set) => ({
   focused: "target",
+  section: 0,
+  setSection: (section) => set({ section }),
   setFocused: (focused) => set({ focused: focused }),
 }));
 
